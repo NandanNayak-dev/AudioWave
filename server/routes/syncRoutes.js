166c35daac78758bc1125c5c5f7e87a2a1d67f09
Nandan Nayak <nandannayak2102@gmail.com>
2026-08-16 12:06:54 +0530

    refactor: remove deprecated and unused utility files

--- server/routes/syncRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const syncController = require('../controllers/syncController');
-
-router.get('/pull/:userId', syncController.pullLibrary);
-router.post('/push', syncController.pushLibrary);
-
-module.exports = router;
